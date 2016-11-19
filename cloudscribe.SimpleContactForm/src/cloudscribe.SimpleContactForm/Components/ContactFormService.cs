1fae476cd1b5084314230ca9d88afa5f539a14c5
Joe Audette <joeaudette@users.noreply.github.com>
2016-11-19 11:58:12 -0500

    wip on contact form

---
+++ cloudscribe.SimpleContactForm/src/cloudscribe.SimpleContactForm/Components/ContactFormService.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Source Tree Solutions, LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. 
+// Author:					Joe Audette
+// Created:					2016-11-19
+// Last Modified:			2016-11-19
+// 
+
+using cloudscribe.SimpleContactForm.Models;
+using cloudscribe.SimpleContactForm.ViewModels;
+using cloudscribe.Web.Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cloudscribe.SimpleContactForm.Components
+{
+    public class ContactFormService
+    {
+        public ContactFormService(
+            IFormIdProvider formIdProvider,
+            IRecaptchaKeysProvider recaptchaKeysProvider
+            )
+        {
+            this.formIdProvider = formIdProvider;
+            recaptchaKeys = recaptchaKeysProvider;
+        }
+
+        private IFormIdProvider formIdProvider;
+        private IRecaptchaKeysProvider recaptchaKeys;
+
+        public async Task<string> GetFormId()
+        {
+            return await formIdProvider.GetFormId().ConfigureAwait(false);
+        }
+
+        public async Task<RecaptchaKeys> GetRecaptchaKeys()
+        {
+            return await recaptchaKeys.GetKeys().ConfigureAwait(false);
+        }
+
+        public Task<MessageResult> ProcessMessage(MessageViewModel model)
+        {
+            var result = MessageResult.Success;
+            // TODO: implement
+
+            return Task.FromResult(result);
+        }
+    }
+}
---
+++ cloudscribe.SimpleContactForm/src/cloudscribe.SimpleContactForm/StartupExtensions.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Source Tree Solutions, LLC. All rights reserved.
+// Licensed under the Apache License, Version 2.0. 
+// Author:					Joe Audette
+// Created:					2016-11-19
+// Last Modified:			2016-11-19
+// 
+
+using cloudscribe.SimpleContactForm.Components;
+using cloudscribe.SimpleContactForm.Models;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddCloudscribeSimpleContactForm(this IServiceCollection services)
+        {
+            services.TryAddScoped<IFormIdProvider, DefaultFormIdProvider>();
+            services.AddScoped<ContactFormService, ContactFormService>();
+
+
+            return services;
+        }
+    }
+}
